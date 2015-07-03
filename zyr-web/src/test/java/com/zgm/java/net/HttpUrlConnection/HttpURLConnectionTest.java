981f49ea671a65c23e00ab0d4638f78a57d1bc52
zgmzyr <84169381@qq.com>
2015-07-03 15:43:48 +0800

    HttpURLConnection Test

--- zyr-web/src/test/java/com/zgm/java/net/HttpUrlConnection/HttpURLConnectionTest.java
+++ zyr-web/src/test/java/com/zgm/java/net/HttpUrlConnection/HttpURLConnectionTest.java
@@ -7,105 +7,60 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
 
-import org.junit.Test;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSON;
 
-/**
- * @author zhouguangming 创建时间: 2014年5月28日 下午7:30:11
- */
 public class HttpURLConnectionTest {
 
-	@Test
-	public void test() {
-		final String spec = "http://kaijiang.500.com/static/info/kaijiang/xml/pls/list.xml";
-		BufferedReader bufferedReader = null;
-		try {
-			URL url = new URL(spec);
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setDoInput(true);
-			// conn.setInstanceFollowRedirects(true);
-			conn.setRequestProperty("content-type", "text/xml;charset=utf-8"); // TODO
-			conn.connect();
-			bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-			String temp = null;
-			int count = 0;
-			while ((temp = bufferedReader.readLine()) != null) {
-				count++;
-				if (count == 3) {
-					System.out.println(temp);
-					break;
-				}
-			}
-
-			temp = temp.trim();
-			int a = temp.indexOf("expect=");
-			int b = temp.indexOf("\" opencode=\"");
-			int c = temp.indexOf("\" opentime=\"");
-			int d = temp.indexOf("\" />");
-			String currentissueNo = temp.substring(a + 8, b);
-			String resultNo = temp.substring(b + 12, c);
-			String openTime = temp.substring(c + 12, d);
-
-			System.out.println(currentissueNo);
-			System.out.println(resultNo);
-			System.out.println(openTime);
-		} catch (IOException e) {
-			// TODO
-		} finally {
-			if (bufferedReader != null) {
-				try {
-					bufferedReader.close();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-				}
-			}
-		}
-	}
+//	private static final Logger log = LoggerFactory.getLogger(OpenAwardController.class);
+	//TODO
+	private static final String GD11X5KJ_URL = "http://www.xxx.com/lottery/kc!kc11x5.jhtml?gameIndex=305";
 	
-	@Test
-	public void test2() {
-		final String spec = "http://www.aicai.com/lottery/kc!kc11x5.jhtml?gameIndex=305";
+	@RequestMapping(value = "/queryDrawNumber")
+	@ResponseBody
+	public String queryDrawNumber(@RequestParam("issueNo") String issueNo) {
 		BufferedReader bufferedReader = null;
+		String drawNumberJsonString = "{'drawNumber':''}";
+		StringBuilder sb = new StringBuilder(800);
 		try {
-			URL url = new URL(spec);
+			URL url = new URL(GD11X5KJ_URL);
 			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("GET");
 			conn.setUseCaches(false);
-			conn.setDoInput(true);
-			// conn.setInstanceFollowRedirects(true);
 			conn.setRequestProperty("content-type", "text/json;charset=utf-8"); // TODO
 			conn.connect();
 			bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 			String temp = null;
-			int count = 0;
-			StringBuilder sb = new StringBuilder(800);
 			while ((temp = bufferedReader.readLine()) != null) {
 				sb.append(temp);
 			}
-			
-			System.out.println(sb);
-			
-			@SuppressWarnings("unchecked")
-			Map<String, Object> parseObject = JSON.parseObject(sb.toString(), Map.class);
-			System.out.println(parseObject);
-			String drawNumber = (String) parseObject.get("result");
-			System.out.println("{'drawNumber':'" + drawNumber + "'}");
-			System.out.println(JSON.toJSONString(drawNumber, false));
-			System.out.println(JSON.parse(drawNumber));
-			
-
 		} catch (IOException e) {
-			// TODO
+//			log.error("{msg:'从" + GD11X5KJ_URL + "获取广东11选五开奖结果时发生异常！'}", e);
 		} finally {
 			if (bufferedReader != null) {
 				try {
 					bufferedReader.close();
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
+//					log.error("{msg:'关闭IO时发生异常！'}");
 				}
 			}
 		}
+		
+		try {
+			@SuppressWarnings("unchecked")
+			Map<String, Object> parseObject = JSON.parseObject(sb.toString(), Map.class);
+			String realIssueNo = (String) parseObject.get("openTerm");
+			if (realIssueNo.equals(issueNo)) {
+				String drawNumber = (String) parseObject.get("result");
+				drawNumberJsonString = "{'drawNumber':'" + drawNumber + "'}";
+			}
+		} catch (Throwable t) {
+//			log.error("{msg:'从" + GD11X5KJ_URL + "获取广东11选五开奖结果后，解析时发生异常！'}", t);
+		}
+		
+		return drawNumberJsonString;
 	}
 }
